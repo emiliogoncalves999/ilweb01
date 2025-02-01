6f43ca1422942da5dd15f8d77b9ecbf2c87ca382
felisbertoemiliogoncalves <167517234+felisbertoemiliogoncalves@users.noreply.github.com>
2025-02-01 22:08:27 +0900

    up
    up

--- visitor/signals.py
+++ visitor/signals.py
@@ -5,53 +5,53 @@ from log.models import LogEntry
 
 User = get_user_model()
 
-# @receiver(post_save)
-# def log_save(sender, instance, created, **kwargs):
-#     if sender == LogEntry:
-#         return
+@receiver(post_save)
+def log_save(sender, instance, created, **kwargs):
+    if sender == LogEntry:
+        return
 
-#     action = 'CREATE' if created else 'UPDATE'
-#     user = kwargs.get('user')
+    action = 'CREATE' if created else 'UPDATE'
+    user = kwargs.get('user')
 
-#     # Attempt to retrieve user if not provided
-#     if user is None:
-#         user_id = getattr(instance, 'user_id', None)
-#         if user_id:
-#             try:
-#                 user = User.objects.get(pk=user_id)
-#             except User.DoesNotExist:
-#                 user = None
+    # Attempt to retrieve user if not provided
+    if user is None:
+        user_id = getattr(instance, 'user_id', None)
+        if user_id:
+            try:
+                user = User.objects.get(pk=user_id)
+            except User.DoesNotExist:
+                user = None
 
-#     # Log the action
-#     LogEntry.objects.create(
-#         action=action,
-#         model_name=sender.__name__,
-#         instance_id=str(instance.pk),
-#         user=user,
-#         details=f"Data: {instance.__dict__}"
-#     )
+    # Log the action
+    LogEntry.objects.create(
+        action=action,
+        model_name=sender.__name__,
+        instance_id=str(instance.pk),
+        user=user,
+        details=f"Data: {instance.__dict__}"
+    )
 
-# @receiver(post_delete)
-# def log_delete(sender, instance, **kwargs):
-#     if sender == LogEntry:
-#         return
+@receiver(post_delete)
+def log_delete(sender, instance, **kwargs):
+    if sender == LogEntry:
+        return
 
-#     user = kwargs.get('user')
+    user = kwargs.get('user')
 
-#     # Attempt to retrieve user if not provided
-#     if user is None:
-#         user_id = getattr(instance, 'user_id', None)
-#         if user_id:
-#             try:
-#                 user = User.objects.get(pk=user_id)
-#             except User.DoesNotExist:
-#                 user = None
+    # Attempt to retrieve user if not provided
+    if user is None:
+        user_id = getattr(instance, 'user_id', None)
+        if user_id:
+            try:
+                user = User.objects.get(pk=user_id)
+            except User.DoesNotExist:
+                user = None
 
-#     # Log the action
-#     LogEntry.objects.create(
-#         action='DELETE',
-#         model_name=sender.__name__,
-#         instance_id=str(instance.pk),
-#         user=user,
-#         details=f"Data: {instance.__dict__}"
-#     )
+    # Log the action
+    LogEntry.objects.create(
+        action='DELETE',
+        model_name=sender.__name__,
+        instance_id=str(instance.pk),
+        user=user,
+        details=f"Data: {instance.__dict__}"
+    )
